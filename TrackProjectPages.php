7b11014218b606f1f17c645823ad5a18f44dd2fc
CptDaniel <daniel.reich@uni-ulm.de>
2012-02-23 09:29:22 +0100

    First commit. Function for dynamical listing of given pages.

--- TrackProjectPages.php
+++ TrackProjectPages.php
@@ -21,6 +21,32 @@ if ((include './config.php') != 'OK') {
     require './configDefault.php';
 }
 
+function curl_get_Wiki($url){
+	
+	$default_opts = array(CURLOPT_URL => $url, 
+						  CURLOPT_HEADER => false, 
+						  CURLOPT_RETURNTRANSFER => 1,
+						  CURLOPT_USERAGENT => "TAOBot"
+						  );
+	$ch = curl_init();
+	curl_setopt_array($ch, $default_opts);
+	
+	$string = curl_exec($ch);
+curl_close($ch);
+$stack = array();
+$unsTaoPrefix = unserialize($string);
+foreach ($unsTaoPrefix as $querry) {
+	foreach ($querry as $alllinks){
+		foreach ($alllinks as $var){
+			array_push($stack,$var["title"]);
+		}
+	}
+}
+var_dump($stack);
+return $stack;
+}
+
+
 var_dump($instance);
 var_dump($user);
 var_dump($project);
